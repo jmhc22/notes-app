885ed6d05cc106cdc30c3f2d659996ae18a67180
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-10-30 13:36:23 +0000

    Adds note-controller.js

---
+++ note-controller.js
@@ -0,0 +1,12 @@
+console.log('test')
+
+var element = document.getElementById("test")
+
+console.log(element)
+
+const content = element.innerHTML;
+
+console.log(content)
+
+
+element.innerHTML = "howdy"
